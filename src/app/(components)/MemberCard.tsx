6ee4a0ff8f49c638a6c3c4db59404c3151ace472
Jasper <ngchingwei253@gmail.com>
2023-12-07 11:31:08 +0800

    added xue jou to the meet our team page

--- src/app/(components)/MemberCard.tsx
+++ src/app/(components)/MemberCard.tsx
@@ -15,7 +15,7 @@ export default function MemberCard({
         <>
           <div className="h-[12rem] w-full bg-orange-400"></div>
           <div
-            className={`absolute inset-0 bg-[url('/chingwei-3.png')] bg-cover bg-no-repeat bg-center`}
+            className={`absolute inset-0 bottom-[-1px] bg-[url('/chingwei-3.png')] bg-cover bg-no-repeat bg-center`}
           ></div>
         </>
       );
@@ -24,7 +24,7 @@ export default function MemberCard({
         <>
           <div className="h-[12rem] w-full bg-red-400"></div>
           <div
-            className={`absolute inset-0 bg-[url('/chingwei-3.png')] bg-cover bg-no-repeat bg-center`}
+            className={`absolute inset-0 bottom-[-1px] bg-[url('/chingwei-3.png')] bg-cover bg-no-repeat bg-center`}
           ></div>
         </>
       );
@@ -33,7 +33,7 @@ export default function MemberCard({
         <>
           <div className="h-[12rem] w-full bg-blue-400"></div>
           <div
-            className={`absolute inset-0 bg-[url('/chingwei-3.png')] bg-cover bg-no-repeat bg-center`}
+            className={`absolute inset-0 bottom-[-1px] bg-[url('/chingwei-3.png')] bg-cover bg-no-repeat bg-center`}
           ></div>
         </>
       );
@@ -41,9 +41,11 @@ export default function MemberCard({
       return (
         <>
           <div className="h-[12rem] w-full bg-purple-400"></div>
-          <div
-            className={`absolute inset-0 bg-[url('/chingwei-3.png')] bg-cover bg-no-repeat bg-center`}
-          ></div>
+          <div className="absolute inset-0 w-full h-full bottom-0 top-0 flex justify-center">
+            <div
+              className={`bg-[url('/xj-2.png')] brightness-105 bg-cover w-[10rem] bg-no-repeat bg-[center_top_0.7rem]`}
+            ></div>
+          </div>
         </>
       );
     } else if (name === "Heng Shu Ci") {
@@ -51,7 +53,7 @@ export default function MemberCard({
         <>
           <div className="h-[12rem] w-full bg-green-400"></div>
           <div
-            className={`absolute inset-0 bg-[url('/chingwei-3.png')] bg-cover bg-no-repeat bg-center`}
+            className={`absolute inset-0 bottom-[-1px] bg-[url('/chingwei-3.png')] bg-cover bg-no-repeat bg-center`}
           ></div>
         </>
       );
@@ -70,7 +72,7 @@ export default function MemberCard({
       <div className="relative w-[15rem] h-[15rem] flex items-end">
         {getImage()}
       </div>
-      <div className="text-white flex flex-col justify-center items-center py-5">
+      <div className="text-white flex flex-col justify-center items-center py-5 ">
         <h1 className="text-md font-bold tracking-wider">{name}</h1>
         <p className="text-xs font-light">{title}</p>
       </div>
